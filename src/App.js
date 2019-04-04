a30ddf8e6e27c43f41faedc622f8f02a8dafdc28
CALuthfi <choiriahmadluthfi@gmail.com>
2019-04-04 17:06:03 +0700

    Multiple Components in Sync

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Navbar from "./mapel/composing-components/navbar/navbar";
+import Counters from "./mapel/composing-components/Removing-the-Local-State/counters";
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <React.Fragment>
+        <Navbar />
+        <main className="container">
+          <Counters />
+        </main>
+      </React.Fragment>
     );
   }
 }
