42d7a9540c24d9c8bebde4deb3b3ce03e1d3b22e
CALuthfi <choiriahmadluthfi@gmail.com>
2019-04-05 09:33:47 +0700

    Destructuring-Arguments

--- src/mapel/composing-components/Stateless-Functional-Components/navbar.jsx
+++ src/mapel/composing-components/Stateless-Functional-Components/navbar.jsx
@@ -1,12 +1,12 @@
 import React, { Component } from "react";
 
-const Navbar = ({ totalCounters }) => {
+const Navbar = props => {
   return (
     <nav className="navbar navbar-light bg-light">
       <a className="navbar-brand" href="#">
         Navbar{" "}
         <span className="badge badge-pill badge-secondary">
-          {totalCounters}
+          {props.totalCounters}
         </span>
       </a>
     </nav>
